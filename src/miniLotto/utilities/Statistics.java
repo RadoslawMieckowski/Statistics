2b469fd3931f606a86138b938c78f0347dbbacfc
Radosław Mieckowski <rmieckowski@wp.pl>
2022-05-21 20:52:25 +0200

    fix calcCosDistance, findQ3 methods in Statistics.java

--- src/miniLotto/utilities/Statistics.java
+++ src/miniLotto/utilities/Statistics.java
@@ -17,7 +17,7 @@ public class Statistics {
         }
         else
         {
-            m = (list.get(size/2-1) + list.get(size/2))/2;
+            m =(list.get(size/2-1) + list.get(size/2))/2.;//albo (double)
         }
         return m;
     }
@@ -28,11 +28,13 @@ public class Statistics {
         double m;
         if(size % 2 == 1)
         {
-            m = list.get(3 * (size+1)/4 - 1);
+            int afterMedianIndex = list.size() / 2 + 1;
+            List<Integer> sublist = list.subList(afterMedianIndex, list.size());
+            m = findMedian(sublist);
         }
         else
         {
-            m = (list.get(3 * size/4 - 1) + list.get(3 * size/4))/2;
+            m = (list.get((int)(3 * size/4. - 1)) + list.get((int)((3 * size + 4)/4.)))/2.;
         }
         return m;
     }
@@ -66,20 +68,21 @@ public class Statistics {
 
         for (int i = 0; i < list1.size(); i++) {
             liczebnik += (list1.get(i) * list2.get(i));
-            składnik_mianownika1 +=  list1.get(i);
-            składnik_mianownika2 +=  list2.get(i);
+            składnik_mianownika1 +=  pow(list1.get(i), 2);
+            składnik_mianownika2 +=  pow(list2.get(i), 2);
         }
         pierwiastek1 = sqrt(składnik_mianownika1);
         pierwiastek2 = sqrt(składnik_mianownika2);
-        mianownik = pierwiastek1 + pierwiastek2;
+        mianownik = pierwiastek1 * pierwiastek2;
         cosDistance = liczebnik / mianownik;
         return cosDistance;
     }
 
     public static void main(String[] args) {
-        List<Integer> listEven = new LinkedList<>(List.of(1, 2, 3, 4));
-        System.out.println(findQ3(listEven));
-        List<Integer> listOdd = new LinkedList<>(List.of(1, 2, 3, 4, 5));
-        System.out.println(findQ3(listOdd));
+        List<Integer> listEven = new LinkedList<>(List.of(23,56,1,2,4,89));
+//        System.out.println(findMedian(listEven));
+        List<Integer> listOdd = new LinkedList<>(List.of(23,56,1,4,89));
+//        System.out.println(findMedian(listOdd));
+        System.out.println(cosDistance(listOdd, listOdd));
     }
 }
