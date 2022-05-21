9f620aabedb352b56cee7fe8fb3e49d701c314b2
Radosław Mieckowski <rmieckowski@wp.pl>
2022-05-21 21:14:53 +0200

    add Two.java to models directory

---
+++ src/miniLotto/models/Two.java
@@ -0,0 +1,12 @@
+package miniLotto.models;
+
+public class Two {
+    int firstNumber;
+    int secondNumber;
+    private int maxInterval;
+    private int minInterval;
+    private int avgInterval;
+    private double medOfintervals;
+    private double q3Ofintervals;
+    int lastOccurence;
+}
