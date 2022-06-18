3bab7ceb33565a8b1ce022232c79029597a6c897
Radosław Mieckowski <rmieckowski@wp.pl>
2022-06-18 18:21:20 +0200

    finish FinderDemo.java

--- src/main/java/miniLotto/demos/FinderDemo.java
+++ src/main/java/miniLotto/demos/FinderDemo.java
@@ -1,5 +1,6 @@
 package miniLotto.demos;
 
+import lombok.SneakyThrows;
 import miniLotto.models.Two;
 import miniLotto.utilities.*;
 
@@ -8,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 public class FinderDemo {
+    @SneakyThrows
     public static void main(String[] args) {
         List<Two> deserializedList = Serializer.deserialize("src/main/resources/Twos_serialized.ser");
         //Two pair = Finder.findTwoWithMaxLastOccurence(Filter.filterTwosWithLastBiggerthan3Q(deserializedList));
@@ -25,10 +27,18 @@ public class FinderDemo {
         System.out.println("minimalny trzeci kwartyl\n" + twoWithMin3Q);
         List<Map<String, Double>> listOfSimilarities = Serializer.deserializeListOfMaps(
                 "src/main/resources/list_of_mapped_distances.ser");
-        List<Map.Entry<String, Double>> filteredMap = Finder.filterListOfMapsByFirstAndSecondNumber(
+        List<Map.Entry<String, Double>> listOfFilteredMaps = Finder.filterListOfMapsByFirstAndSecondNumber(
                 String.valueOf(twoWithMin3Q.getFirstNumber()),
                 String.valueOf(twoWithMin3Q.getSecondNumber()),
                 listOfSimilarities
                 );
+        List<Map.Entry<String, Double>> listOfFiveMostSimilarEntries =
+                Finder.findMostSimilarEntriesWithGivenTwo(listOfFilteredMaps, 5);
+
+        System.out.println("\n5 najbardziej podobnych dwójek:");
+        Presenter.presentList(listOfFiveMostSimilarEntries);
+
+        //można brać ostatnie losowanie i zamiast twoWithMin3Q postawiać odpowiednie wszystkie dwójki z ostatniego losowania
+     // zaautomatyzuj to poprzez dodanie generatora Two, który bierze podane liczby i składa z nich obiekty Two
     }
 }
