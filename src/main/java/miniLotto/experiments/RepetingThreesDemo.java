fc2cdf4908615a4a6137d1d62c648cb8f52b88ab
Radosław Mieckowski <rmieckowski@wp.pl>
2022-11-11 21:03:53 +0100

    calculate statistics for winningDistances in RepetingThreesDemo.java

--- src/main/java/miniLotto/experiments/RepetingThreesDemo.java
+++ src/main/java/miniLotto/experiments/RepetingThreesDemo.java
@@ -1,10 +1,10 @@
 package miniLotto.experiments;
 
+import miniLotto.utilities.Statistics;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 
 public class RepetingThreesDemo {
     static int oldTab[]=new int[5];
@@ -15,14 +15,34 @@ public class RepetingThreesDemo {
 
     public static void main(String[] args) {
         winningDistances = new LinkedList<>();
-        Scanner in = wczytywanie();
-        lookForThreesAndMakeWinningDistancesList(in);
+        Scanner in = readFile("src/main/java/miniLotto/wyniki-minilotto-sortowane-parsed.csv");
+        lookForThreesAndMakeWinningDistancesList(in);// mamy obliczone znalezione winning distances
+        System.out.println();
+        System.out.println("==========================================");
+        System.out.println("interwały znalezionych przypadków: ");
+        System.out.println(winningDistances);
+        double median = Statistics.findMedian(winningDistances);
+        System.out.println("Mediana zbioru: " + median);
+        double q3 = Statistics.findQ3(winningDistances);
+        System.out.println("Trzeci kwantyl zbioru: " + q3);
+        double lastOccurence = winningDistances.getLast();// zależy nam na ostatnim przypadku nieposortowanego zbioru
+        System.out.println("Ostatni przypadek: " + lastOccurence);
+        double avg = winningDistances.stream()
+                .mapToInt(Integer::intValue)
+                .average()
+                .getAsDouble();
+        System.out.println("Średni interwał: " + avg);
+        System.out.println("posortowane interwały znalezionych przypadków: ");
+        List<Integer> winningDistancesCopy = new ArrayList<>(winningDistances);
+        Collections.sort(winningDistancesCopy);
+        System.out.println(winningDistancesCopy);
+        in.close();
     }
 
-    static Scanner wczytywanie() {
+    static Scanner readFile(String fileName) {
         Scanner in = null;
         try {
-            in = new Scanner(new File("src/main/java/miniLotto/wyniki-minilotto-sortowane-parsed.csv"));
+            in = new Scanner(new File(fileName));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -38,22 +58,24 @@ public class RepetingThreesDemo {
             newTab[i]=in.nextInt();
         }
         System.out.println(Arrays.toString(newTab));
-        findingWinningDistances();
+
+        findingWinningDistancesAndCountWins();
         System.out.println(counter);
 
-        while(in.hasNextLine()){
-            oldTab=Arrays.copyOf(newTab,5);
+        while (in.hasNextLine()) {
+            oldTab = Arrays.copyOf(newTab,5);
             System.out.println(Arrays.toString(oldTab));
-            for (int i=0;i<5;i++){
-                newTab[i]=in.nextInt();
+            for (int i = 0; i < 5; i++){
+                newTab[i] = in.nextInt();
             }
             System.out.println(Arrays.toString(newTab));
-            findingWinningDistances();
-            System.out.println(counter);
+            findingWinningDistancesAndCountWins();
+            System.out.println("liczba znalezionych przypadków (trójek z poprzedniego losowania): "
+                    + counter);
         }
     }
 
-    static void findingWinningDistances(){
+    static void findingWinningDistancesAndCountWins(){
         boolean isSuccess = false;
         int counterThree =0;
         stop:
