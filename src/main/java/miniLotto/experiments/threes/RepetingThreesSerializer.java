5bdc99fdba42fd43a54ca83439a1f2d5fbae1613
Radosław Mieckowski <rmieckowski@wp.pl>
2022-11-13 15:52:36 +0100

    fix RepetingThreesSerializer.java

--- src/main/java/miniLotto/experiments/threes/RepetingThreesSerializer.java
+++ src/main/java/miniLotto/experiments/threes/RepetingThreesSerializer.java
@@ -1,5 +1,6 @@
 package miniLotto.experiments.threes;
 
+import miniLotto.utilities.Presenter;
 import miniLotto.utilities.Serializer;
 import miniLotto.utilities.Statistics;
 
@@ -13,19 +14,25 @@ public class RepetingThreesSerializer {
     static int counter=0;
     static LinkedList<Integer> winningDistances;
     static int counterOfDistancesBetweenThrees = 1;
-    static LinkedList<int[]> oldTabListToSerialize = new LinkedList<>();
-    static LinkedList<int[]> newTabListToSerialize = new LinkedList<>();
+    static LinkedList<int[]> oldTabListToSerialize;
+    static LinkedList<int[]> newTabListToSerialize;
     final static String OLD_TAB_LIST_SER_FILE ="src/main/resources/oldTabListToSerialize.ser";
     final static String NEW_TAB_LIST_SER_FILE ="src/main/resources/newTabListToSerialize.ser";
 
     public static void main(String[] args) {
         winningDistances = new LinkedList<>();
+        oldTabListToSerialize = new LinkedList<>();
+        newTabListToSerialize = new LinkedList<>();
         Scanner in = readFile("src/main/java/miniLotto/wyniki-minilotto-sortowane-parsed.csv");
         lookForThreesAndMakeWinningDistancesList(in);// mamy obliczone znalezione winning distances
         System.out.println();
         System.out.println("==========================================");
         Statistics.showStatistics(winningDistances);
         in.close();
+//        System.out.println("tuż przed serializacją: ");
+//        Presenter.presentListOfIntTables(newTabListToSerialize);
+//        System.out.println();
+//        Presenter.presentListOfIntTables(oldTabListToSerialize);
         Serializer.serialize(oldTabListToSerialize, OLD_TAB_LIST_SER_FILE);
         Serializer.serialize(newTabListToSerialize, NEW_TAB_LIST_SER_FILE);
     }
@@ -44,25 +51,26 @@ public class RepetingThreesSerializer {
         for (int i = 0; i < 5; i++){
             oldTab[i] = in.nextInt();
         }
-        System.out.println(Arrays.toString(oldTab));
+//        System.out.println(Arrays.toString(oldTab));
         for (int i = 0; i < 5; i++){
             newTab[i] = in.nextInt();
         }
-        System.out.println(Arrays.toString(newTab));
+//        System.out.println(Arrays.toString(newTab));
 
         findingWinningDistancesAndCountWins();
         System.out.println(counter);
 
         while (in.hasNextLine()) {
             oldTab = Arrays.copyOf(newTab,5);
-            System.out.println(Arrays.toString(oldTab));
+//            System.out.println(Arrays.toString(oldTab));
+            newTab = new int[5];
             for (int i = 0; i < 5; i++){
                 newTab[i] = in.nextInt();
             }
-            System.out.println(Arrays.toString(newTab));
+//            System.out.println(Arrays.toString(newTab));
             findingWinningDistancesAndCountWins();
-            System.out.println("liczba znalezionych przypadków (trójek z poprzedniego losowania): "
-                    + counter);
+          /*  System.out.println("liczba znalezionych przypadków (trójek z poprzedniego losowania): "
+                    + counter);*/
         }
     }
 
@@ -79,8 +87,11 @@ public class RepetingThreesSerializer {
                         isSuccess = true;
                         winningDistances.add(counterOfDistancesBetweenThrees);
                         counterOfDistancesBetweenThrees = 1;
+                        System.out.println(Arrays.toString(oldTab));
                         oldTabListToSerialize.add(oldTab);
+                        System.out.println(Arrays.toString(newTab));
                         newTabListToSerialize.add(newTab);
+                        System.out.println();
                         break stop;
                     }
                 }
